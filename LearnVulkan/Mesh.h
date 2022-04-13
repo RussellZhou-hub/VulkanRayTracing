59511af36246c5fd58dd60dd602b8788dd2ba833
RussellZhou-Hub <938311288@qq.com>
2022-04-13 15:33:58 +0800

    update mesh.h

--- LearnVulkan/Mesh.h
+++ LearnVulkan/Mesh.h
@@ -1,72 +1,31 @@
 #pragma once
 
-#ifndef MESH_H
-#define MESH_H
-
-#define _CRT_SECURE_NO_WARNINGS
-#define SEEK_END 2
-#define VK_ENABLE_BETA_EXTENSIONS
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-#include <vulkan/vulkan.h>
-
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-
-#include <string>
+#include "vk_types.h"
 #include <vector>
-#include <iostream>
-#include <fstream>
-#include <stdexcept>
-#include <algorithm>
-#include <chrono>
-#include <cstring>
-#include <array>
-#include <set>
-#include <unordered_map>
-using namespace std;
+#include<vulkan/vulkan.h>
+#include <glm/vec3.hpp>
 
-#define MAX_BONE_INFLUENCE 4
+struct VertexInputDescription {
+
+    std::vector<VkVertexInputBindingDescription> bindings;
+    std::vector<VkVertexInputAttributeDescription> attributes;
+
+    VkPipelineVertexInputStateCreateFlags flags = 0;
+};
 
 struct Vertex {
-    glm::vec3 Position;
-    glm::vec3 Normal;
-    glm::vec2 TexCoords;
-    glm::vec3 Tangent;
-    // bitangent
-    glm::vec3 Bitangent;
-    //bone indexes which will influence this vertex
-    int m_BoneIDs[MAX_BONE_INFLUENCE];
-    //weights from each bone
-    float m_Weights[MAX_BONE_INFLUENCE];
 
-    static VkVertexInputBindingDescription getBindingDescription();
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec3 color;
 
-    static std::array<VkVertexInputAttributeDescription, 3> getAttributeDescriptions();
-
-    bool operator==(const Vertex& other) const;
+    static VertexInputDescription get_vertex_description();
 };
 
-struct Texture {
-    unsigned int id;
-    string type;
-    string path;
+struct Mesh {
+    std::vector<Vertex> _vertices;
+
+    AllocatedBuffer _vertexBuffer;
+
+    bool load_from_obj(const char* filename);
 };
-
-class Mesh {
-public:
-    // mesh data
-    vector<Vertex>       vertices;
-    vector<unsigned int> indices;
-    vector<Texture>      textures;
-
-    Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures);
-    void Draw();
-private:
-    //  render data
-    unsigned int VAO, VBO, EBO;
-
-    void setupMesh();
-};
-
-#endif
